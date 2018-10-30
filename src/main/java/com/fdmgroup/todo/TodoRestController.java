b6ca7959d85beb1cb2ff62b317c1c0c36279ffa2
CharlieBotello <charlieabotello@gmail.com>
2018-10-30 11:12:15 -0400

    update

--- src/main/java/com/fdmgroup/todo/TodoRestController.java
+++ src/main/java/com/fdmgroup/todo/TodoRestController.java
@@ -3,6 +3,7 @@ package com.fdmgroup.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class TodoRestController {
 	
 	@RequestMapping(path="/todos")
 	public List<Todo> retrieveAllTodos(){
-		return service.retrieveTodos("charlie");
+		return service.retrieveTodos("fmdLearning");
+	}
+	
+	@RequestMapping(path="/todos/{id}")
+	public Todo retrieveTodo(@PathVariable int id){
+		return service.retrieveTodo(id);
 	}
 }
